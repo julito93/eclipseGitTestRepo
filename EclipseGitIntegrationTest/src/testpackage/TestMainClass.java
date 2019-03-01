071f25ebe9a4a7a3eb7abbab207adec5ce4e186d
jcampaz <jcampaz@JCAMPAZ-PC.cafeto.local>
2019-02-28 22:34:04 -0500

    Changed the Console interactions by pop up panes

--- EclipseGitIntegrationTest/src/testpackage/TestMainClass.java
+++ EclipseGitIntegrationTest/src/testpackage/TestMainClass.java
@@ -7,6 +7,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import javax.swing.JOptionPane;
+
 /**
  * @author jcampaz
  *
@@ -19,11 +21,16 @@ public class TestMainClass {
 	 */
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		System.out.println("Hello from the console!");
-		System.out.println("How are you?");
-		BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
-		String answer=input.readLine();
-		System.out.println("I'm " +answer + " too");
+//		System.out.println("Hello from the console!");
+//		System.out.println("How are you?");
+//		BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
+		JOptionPane.showMessageDialog(null, "Hello from the message dialog!");
+		JOptionPane.showMessageDialog(null, "How are you?");
+		String answer;
+		answer=JOptionPane.showInputDialog(null);
+		JOptionPane.showMessageDialog(null, "I'm "+answer+" too");
+		
+//		System.out.println("I'm " +answer + " too");
 	}
 
 }
